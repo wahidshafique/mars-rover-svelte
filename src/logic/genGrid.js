3d59da750aa6884a3437bfc9009aaa7988d359d1
wahidshafique <wahid.shafique@gubagoo.com>
2020-12-23 11:24:41 -0500

    use unused assets for grid for fun

--- src/logic/genGrid.js
+++ src/logic/genGrid.js
@@ -1,11 +1,34 @@
 import Tiles from '../assets';
 
-const genGrid = (gridx, gridy, rovers) => {
+const tileDecider = (x, y, gridWidth, gridHeight) => {
+  let tile = Tiles.center;
+
+  if (x === gridWidth - 1 && y === 0) {
+    tile = Tiles.topRight;
+  } else if (x === 0 && y === 0) {
+    tile = Tiles.topLeft;
+  } else if (y === gridHeight - 1 && x === 0) {
+    tile = Tiles.bottomLeft;
+  } else if (y === gridHeight - 1 && x === gridWidth - 1) {
+    tile = Tiles.bottomRight;
+  } else if (y === 0) {
+    tile = Tiles.topCenter;
+  } else if (x === 0) {
+    tile = Tiles.left;
+  } else if (x === gridWidth - 1) {
+    tile = Tiles.right;
+  } else if (y === gridHeight - 1) {
+    tile = Tiles.bottomCenter;
+  }
+  return { url: tile };
+};
+
+const genGrid = (gridWidth, gridHeight, rovers) => {
   let tmpGrid = [];
-  for (let y = 0; y < gridy; y++) {
+  for (let y = 0; y < gridHeight; y++) {
     tmpGrid[y] = [];
-    for (let x = 0; x < gridx; x++) {
-      tmpGrid[y][x] = { url: Tiles.center };
+    for (let x = 0; x < gridWidth; x++) {
+      tmpGrid[y][x] = tileDecider(x, y, gridWidth, gridHeight);
       if (rovers.length > 0) {
         const roverAtPosition = rovers.find((r) => r.x === x && r.y === y);
         if (roverAtPosition) {
